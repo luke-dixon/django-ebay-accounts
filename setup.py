c79a2ae375078e181fd31febf96012c7408f56af
Luke Dixon <6b8b4567@gmail.com>
2013-07-18 10:53:09 +0100

    Updated version to 0.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-ebay-accounts',
-    version='0.0.1',
+    version='0.0.2',
     packages=['ebay_accounts'],
     include_package_data=True,
     license='BSD License',
