47bbcbe785ff735a90e0911d3d56a7a442b011e0
Luke Dixon <luke@luke.dixon.name>
2016-06-18 08:04:44 +0100

    Define an AppConfig for this app

--- ebay_accounts/__init__.py
+++ ebay_accounts/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-"""
-
-"""
 from __future__ import unicode_literals
 
+
+default_app_config = 'ebay_accounts.apps.EbayAccountsConfig'
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 fileencoding=utf-8
--- ebay_accounts/apps.py
+++ ebay_accounts/apps.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from django.apps import AppConfig
+
+
+class EbayAccountsConfig(AppConfig):
+    name = 'ebay_accounts'
+    verbose_name = 'ebay accounts'
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
