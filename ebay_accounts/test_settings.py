98e876b03be8204e235c1f17388b70fa60cc2ea7
Luke Dixon <luke@luke.dixon.name>
2016-06-18 06:17:08 +0100

    Fix test_settings to work with django 1.7

--- ebay_accounts/test_settings.py
+++ ebay_accounts/test_settings.py
@@ -23,8 +23,20 @@ if django.VERSION[:2] < (1, 6):
 
 INSTALLED_APPS += (
     'ebay_accounts',
-    'south',
 )
+if django.VERSION[:2] < (1, 7):
+    INSTALLED_APPS += (
+        'south',
+    )
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
