035ec6724f2a0bf8a473bd0c97efd80c4af3e956
Luke Dixon <luke@luke.dixon.name>
2019-04-11 17:15:38 +0100

    Remove SessionAuthenticationMiddleware from test settings
    since it is enabled unconditionally after Django 1.10.

--- ebay_accounts/test_settings.py
+++ ebay_accounts/test_settings.py
@@ -21,7 +21,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
