dcc2089e29f2f71b94476634aa7405098d113b45
Luke Dixon <luke@luke.dixon.name>
2019-04-13 09:47:04 +0100

    Ensure README file gets closed during setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ import os
 from setuptools import setup
 
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+    README = f.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
